bbc18d886602eae8696e18a428f9a60f73c131cc
Magic17 <this_is_magic@hotmail.com>
2015-05-05 15:05:32 -0400

    first commit test

---
+++ app/src/main/java/com/example/magic/dailysmile/Image_Scroller.java
@@ -0,0 +1,137 @@
+package com.example.magic.dailysmile;
+
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.util.ArrayList;
+
+//test
+
+public class Image_Scroller extends ActionBarActivity implements GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener {
+
+    GestureDetectorCompat gDetector;
+    ImageView image1;
+    ImageView image2;
+    int[] imgs;
+    int i = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image__scroller);
+
+        image1 = (ImageView) findViewById(R.id.mainImage);
+        image2 = (ImageView) findViewById(R.id.adImage);
+
+        imgs = new int[]{R.drawable.ice, R.drawable.manga1, R.drawable.music};
+
+        image1.setImageResource(imgs[i]);
+        image2.setImageResource(R.drawable.music);
+
+        this.gDetector = new GestureDetectorCompat(this, this);
+        gDetector.setOnDoubleTapListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_image__scroller, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+       /*if (i == 2) {
+            i = 0;
+        }
+        else {
+            i += 1;
+        }
+        image1.setImageResource(imgs[i]);
+        */
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        if (i == 2) {
+            i = 0;
+        }
+        else {
+            i += 1;
+        }
+        image1.setImageResource(imgs[i]);
+
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gDetector.onTouchEvent(event);
+        // Be sure to call the superclass implementation
+        return super.onTouchEvent(event);
+    }
+
+}
