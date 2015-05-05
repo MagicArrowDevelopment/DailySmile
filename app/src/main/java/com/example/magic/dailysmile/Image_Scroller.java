3cc588058c6a1ddecaa0f25203052c9f16557496
Magic17 <this_is_magic@hotmail.com>
2015-05-05 16:49:08 -0400

    org push

--- app/src/main/java/com/example/magic/dailysmile/Image_Scroller.java
+++ app/src/main/java/com/example/magic/dailysmile/Image_Scroller.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 
 //test
 // first commit and push
+// org test
 public class Image_Scroller extends ActionBarActivity implements GestureDetector.OnGestureListener,
         GestureDetector.OnDoubleTapListener {
 
